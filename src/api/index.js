aaeef8839a38d977580ad9092a16eee91d23e458
Naman Bansal <namanbansal3284@gmail.com>
2023-06-21 09:41:15 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+            'X-RapidAPI-Key': '77f268467fmsh17d225cc94a7720p149588jsn77f5179218fa',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-rapidapi-key":
+              "777823926dmsha029828a38e3225p11a586jsn0ad1cf0c63e2",
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
